6fb74b56b36cef1ab83d0be4edb39e3f0cfb98df
steel <gawct.231020@gmail.com>
2024-03-05 14:46:03 +0900

    Swipeで移動

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     float speed=0;
+    Vector2 startPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,13 @@ public class CarController : MonoBehaviour
     void Update()
     {
         if(Input.GetMouseButtonDown(0)){
-            this.speed=0.2f;        
+            this.startPos=Input.mousePosition;     
+        }
+        else if(Input.GetMouseButtonUp(0)){
+            Vector2 endPos=Input.mousePosition;
+            float swipeLength=endPos.x-this.startPos.x;
+
+            this.speed=swipeLength/500.0f;
         }
         transform.Translate(this.speed,0,0);
         this.speed*=0.98f;
